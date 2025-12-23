b6a67f13779592356f9fd875bc434ab9bf0d5b09
runeking2006 <gautham.vajiravel@gmail.com>
2025-12-23 22:57:10 +0530

    feat : pie chart size change

--- frontend/src/AnalyticsView.js
+++ frontend/src/AnalyticsView.js
@@ -119,7 +119,7 @@ export default function AnalyticsView({ onBack }) {
                   data={talukData}
                   dataKey="value"
                   nameKey="name"
-                  outerRadius={150}
+                  outerRadius={100}
                   fill="#8884d8"
                   label
                 >
