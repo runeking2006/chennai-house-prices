b2cbd2d4a274282b06ec2dafbad4f9cace3e0e5b
runeking2006 <gautham.vajiravel@gmail.com>
2026-05-16 17:09:49 +0530

    feat:adding floating icons 20

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 import { motion, AnimatePresence } from "framer-motion";
 import { predictPrice, storeFormData } from "./api"; // API helper
 import AnalyticsView from "./AnalyticsView"; // Your analytics component
